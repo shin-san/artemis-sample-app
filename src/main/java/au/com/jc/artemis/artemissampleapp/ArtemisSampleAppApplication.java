1ec230640d1b5c88a81beab9264395159aec2121
JC <jchan@integral.com.au>
2022-07-11 12:50:14 +1000

    first commit

---
+++ src/main/java/au/com/jc/artemis/artemissampleapp/ArtemisSampleAppApplication.java
@@ -0,0 +1,13 @@
+package au.com.jc.artemis.artemissampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtemisSampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/com/jc/artemis/artemissampleapp/ArtemisSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.jc.artemis.artemissampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtemisSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
